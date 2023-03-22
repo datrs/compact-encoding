c1f3d00922696bf7633c8b39a22de3cb67a9ea24
Timo Tiuraniemi <timo.tiuraniemi@iki.fi>
2023-03-22 09:33:06 +0200

    Move code from datrs/hypercore

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+//! Compact encoding module. Rust implementation of https://github.com/compact-encoding/compact-encoding.
+
+pub mod generic;
+pub mod types;
+pub use types::{CompactEncoding, State};
