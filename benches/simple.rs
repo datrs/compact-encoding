1289a939f728d66001944dea580e929a41a038a7
Blake Griffith <blake.a.griffith@gmail.com>
2025-05-02 17:09:50 -0400

    fix benches

--- benches/simple.rs
+++ benches/simple.rs
@@ -1,8 +1,6 @@
 use std::time::Instant;
 
-use compact_encoding::{
-    create_buffer, map_decode, map_encode, sum_encoded_size, CompactEncoding, EncodingError,
-};
+use compact_encoding::{map_decode, map_encode, sum_encoded_size, CompactEncoding, EncodingError};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 const U32_VALUE: u32 = 0xF0E1D2C3;
@@ -13,7 +11,7 @@ fn preencode() -> Result<usize, EncodingError> {
     Ok(sum_encoded_size!(U32_VALUE, STR_VALUE, U64_VALUE))
 }
 
-fn encode(buffer: &mut Box<[u8]>) -> Result<(), EncodingError> {
+fn encode(buffer: &mut [u8]) -> Result<(), EncodingError> {
     let _ = map_encode!(buffer, U32_VALUE, STR_VALUE, U64_VALUE);
     Ok(())
 }
@@ -67,7 +65,7 @@ fn decode_generic_simple(c: &mut Criterion) {
     c.bench_function("decode generic simple", |b| {
         b.iter_custom(|iters| {
             let encoded_size = preencode().unwrap();
-            let mut buffer = fixed_buffer_from_encoded_size(encoded_size);
+            let mut buffer = vec![0_u8; encoded_size];
             encode(&mut buffer).unwrap();
             let start = Instant::now();
             for _ in 0..iters {
